3712f5a67517dc0cd1b3cf993e0fb675a30ed4d3
Josh Parker <jparker@sherpas.ai>
2021-11-30 17:03:29 -0700

    add depth select and max categories select

--- wikipedia/traverse-subcategory-tree.user.js
+++ wikipedia/traverse-subcategory-tree.user.js
@@ -10,12 +10,16 @@
 // ==/UserScript==
 
 (function subcategoryTreeUserScript() {
+  const DEFAULT_DEPTH = 3;
+  const DEFAULT_MAX = 60;
   const parser = new DOMParser();
   const categoryArea = document.querySelector('div#mw-pages > div.mw-content-ltr');
+  const subcategories = document.querySelector('div#mw-subcategories');
   const alreadyRetrieved = new Set();
+  let max;
 
-  const traverseTree = function traverseTree(href) {
-    if (!href || alreadyRetrieved.has(href)) {
+  const traverseTree = function traverseTree(href, depth, parent) {
+    if (!href || depth < 1) {
       return;
     }
 
@@ -23,22 +27,48 @@
       .then(r => r.text())
       .then(text => parser.parseFromString(text, 'text/html'))
       .then(doc => {
-        categoryArea.innerHTML += `<hr><h3>${doc.querySelector('h1#firstHeading').innerText.replace('Category:', '')}</h3>${doc.querySelector('#mw-pages > .mw-content-ltr').innerHTML}`;
-        [...doc.querySelectorAll('div#mw-subcategories ul > li a')].forEach(({ href }) => traverseTree(href));
+        const title = doc.querySelector('h1#firstHeading').innerText.replace('Category:','');
+        categoryArea.innerHTML += `<hr><h3 class="subcategory-title">${title} (parent category: ${parent})</h3>${doc.querySelector('#mw-pages > .mw-content-ltr').innerHTML}`;
+        const children = [...doc.querySelectorAll('div#mw-subcategories ul > li a')].slice(0, max).filter(({ href }) => !alreadyRetrieved.has(href));
+        max -= children.length;
+        children.forEach(({ href }) => traverseTree(href, depth - 1, title));
       });
 
     alreadyRetrieved.add(href);
   };
 
-  const subcategoryHrefs = [...document.querySelectorAll('div#mw-subcategories ul > li a')].map(e => e.href);
+  const subcategoryHrefs = [...subcategories.querySelectorAll('ul > li a')].map(e => e.href);
   const getSubcategoryPagesButton = document.createElement('button');
-  getSubcategoryPagesButton.innerText = 'get subcategory pages';
+  getSubcategoryPagesButton.id = 'get-subcategory-pages';
+  const buttonText = (depth, max) => `get subcategory pages (up to ${depth} layer${depth == 1 ? '' : 's'} deep; max ${max} subcategories)`;
+  getSubcategoryPagesButton.innerText = buttonText(DEFAULT_DEPTH, DEFAULT_MAX);
+
+  const layersInput = document.createElement('input');
+  layersInput.id = 'layers';
+  layersInput.type = 'number';
+  layersInput.value = DEFAULT_DEPTH;
+
+  const maxInput = document.createElement('input');
+  maxInput.id = 'max';
+  maxInput.type = 'number';
+  maxInput.value = DEFAULT_MAX;
+
+  const changeListener = () => { getSubcategoryPagesButton.innerText = buttonText(layersInput.value, maxInput.value); };
+
+  layersInput.addEventListener('change', changeListener);
+  maxInput.addEventListener('change', changeListener);
+
   getSubcategoryPagesButton.addEventListener('click', () => {
     getSubcategoryPagesButton.setAttribute('disabled', true);
-    subcategoryHrefs.forEach(href => traverseTree(href));
+    max = maxInput.value;
+    const children = subcategoryHrefs.slice(0,max);
+    max -= children.length;
+    children.forEach(href => traverseTree(href, layersInput.value, document.querySelector('h1#firstHeading').innerText.replace('Category:','')));
   });
 
   if (subcategoryHrefs.length) {
-    document.querySelector('div#mw-subcategories').appendChild(getSubcategoryPagesButton);
+    subcategories.appendChild(getSubcategoryPagesButton);
+    subcategories.appendChild(layersInput);
+    subcategories.appendChild(maxInput);
   }
 }());
