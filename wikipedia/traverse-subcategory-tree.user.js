a650faa0b7d44577f11067456590e1925fada1eb
Josh Parker <jparker@sherpas.ai>
2021-11-30 17:24:00 -0700

    show the count of subcategories found

--- wikipedia/traverse-subcategory-tree.user.js
+++ wikipedia/traverse-subcategory-tree.user.js
@@ -12,32 +12,7 @@
 (function subcategoryTreeUserScript() {
   const DEFAULT_DEPTH = 3;
   const DEFAULT_MAX = 60;
-  const parser = new DOMParser();
-  const categoryArea = document.querySelector('div#mw-pages > div.mw-content-ltr');
-  const subcategories = document.querySelector('div#mw-subcategories');
-  const alreadyRetrieved = new Set();
-  let maxSubcategories;
 
-  const traverseTree = function traverseTree(url, depth, parent) {
-    if (!url || depth < 1) {
-      return;
-    }
-
-    fetch(url)
-      .then((r) => r.text())
-      .then((text) => parser.parseFromString(text, 'text/html'))
-      .then((doc) => {
-        const title = doc.querySelector('h1#firstHeading').innerText.replace('Category:', '');
-        categoryArea.innerHTML += `<hr><h3 class="subcategory-title">${title} (parent category: ${parent})</h3>${doc.querySelector('#mw-pages > .mw-content-ltr').innerHTML}`;
-        const children = [...doc.querySelectorAll('div#mw-subcategories ul > li a')].slice(0, maxSubcategories).filter(({ href }) => !alreadyRetrieved.has(href));
-        maxSubcategories -= children.length;
-        children.forEach(({ href }) => traverseTree(href, depth - 1, title));
-      });
-
-    alreadyRetrieved.add(url);
-  };
-
-  const subcategoryHrefs = [...subcategories.querySelectorAll('ul > li a')].map(({ href }) => href);
   const getSubcategoryPagesButton = document.createElement('button');
   getSubcategoryPagesButton.id = 'get-subcategory-pages';
   const buttonText = (depth, max) => `get subcategory pages (up to ${depth} layer${(+depth) === 1 ? '' : 's'} deep; max ${max} subcategories)`;
@@ -60,8 +35,38 @@
   layersInput.addEventListener('change', changeListener);
   maxInput.addEventListener('change', changeListener);
 
+  let count = 0;
+  const disabledButtonText = () => { count += 1; return `${count} subcategories found`; };
+  const parser = new DOMParser();
+  const categoryArea = document.querySelector('div#mw-pages > div.mw-content-ltr');
+  let maxSubcategories;
+  const alreadyRetrieved = new Set();
+  const traverseTree = function traverseTree(url, depth, parent) {
+    if (!url || depth < 1) {
+      return;
+    }
+
+    fetch(url)
+      .then((r) => r.text())
+      .then((text) => parser.parseFromString(text, 'text/html'))
+      .then((doc) => {
+        const title = doc.querySelector('h1#firstHeading').innerText.replace('Category:', '');
+        categoryArea.innerHTML += `<hr><h3 class="subcategory-title">${title} (parent category: ${parent})</h3>${doc.querySelector('#mw-pages > .mw-content-ltr').innerHTML}`;
+        const children = [...doc.querySelectorAll('div#mw-subcategories ul > li a')].slice(0, maxSubcategories).filter(({ href }) => !alreadyRetrieved.has(href));
+        maxSubcategories -= children.length;
+        getSubcategoryPagesButton.innerText = disabledButtonText();
+        children.forEach(({ href }) => traverseTree(href, depth - 1, title));
+      });
+
+    alreadyRetrieved.add(url);
+  };
+
+  const subcategories = document.querySelector('div#mw-subcategories');
+  const subcategoryHrefs = [...subcategories.querySelectorAll('ul > li a')].map(({ href }) => href);
   getSubcategoryPagesButton.addEventListener('click', () => {
     getSubcategoryPagesButton.setAttribute('disabled', true);
+    layersInput.setAttribute('disabled', true);
+    maxInput.setAttribute('disabled', true);
     maxSubcategories = maxInput.value;
     const children = subcategoryHrefs.slice(0, maxSubcategories);
     maxSubcategories -= children.length;
