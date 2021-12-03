de96c3839a51c388f2923a7d5c84026d12eab731
Josh Parker <jparker@sherpas.ai>
2021-12-03 12:29:00 -0700

    do basic parse example

--- node-parsing/parse-example.js
+++ node-parsing/parse-example.js
@@ -1,14 +1,24 @@
-import fetch from 'node-fetch';
-import { parse } from 'parse5';
+const fetch = require('node-fetch');
+const { parse } = require('parse5');
 
-const getDescendantByTagName = (node, tagName) => {
-  for (let i = 0; i < node.childNodes.length; i++) {
+const getDescendantByTag = (node, tag) => {
+  for (let i = 0; i < node.childNodes?.length; i++) {
+    if (node.childNodes[i].tagName === tag) return node.childNodes[i];
 
-    
+    const result = getDescendantByTag(node.childNodes[i], tag);
+    if (result) return result;
   }
+
+  return null;
 };
 
 fetch('https://google.com', { redirect: 'manual' })
   .then((response) => response.text())
-  .then((text) => parse(text))
-  .then((dom) => console.log(getDescendantByTagName(dom, 'title')));
+  .then((text) => {
+    console.log(text);
+    const dom = parse(text);
+    console.log(getDescendantByTag(dom, 'title'));
+    console.log(getDescendantByTag(dom, 'span'));
+    console.log(getDescendantByTag(dom, 'a'));
+    console.log(getDescendantByTag(dom, 'div'));
+  });
