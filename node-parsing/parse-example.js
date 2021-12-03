856906123687734c84e7830cccca216b5b9ee739
Josh Parker <jparker@sherpas.ai>
2021-12-03 12:33:28 -0700

    more general parsing in example

--- node-parsing/parse-example.js
+++ node-parsing/parse-example.js
@@ -1,16 +1,20 @@
 const fetch = require('node-fetch');
 const { parse } = require('parse5');
 
-const getDescendantByTag = (node, tag) => {
+const getDescendant = (node, predicate) => {
   for (let i = 0; i < node.childNodes?.length; i++) {
-    if (node.childNodes[i].tagName === tag) return node.childNodes[i];
+    if (predicate(node.childNodes[i])) return node.childNodes[i];
 
-    const result = getDescendantByTag(node.childNodes[i], tag);
+    const result = getDescendant(node.childNodes[i], predicate);
     if (result) return result;
   }
 
   return null;
 };
+
+const getDescendantByTag = (node, tag) => (
+  getDescendant(node, (childNode) => childNode.tagName === tag)
+);
 
 fetch('https://google.com', { redirect: 'manual' })
   .then((response) => response.text())
