b4466c89e143cf957c6935e374ce2d0c1f96cafa
Josh Parker <jparker@sherpas.ai>
2021-12-21 11:59:30 -0700

    get cross origin on more titles

--- cross-origin/get-cross-origin.user.js
+++ cross-origin/get-cross-origin.user.js
@@ -6,27 +6,40 @@
 // @author       Josh Parker
 // @match        https://www.imdb.com/search/title/?year=2021&sort=num_votes,desc
 // @icon         https://www.google.com/s2/favicons?domain=imdb.com
+// @connect      www.rottentomatoes.com
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 (function getRT() {
   const rtRoot = 'https://www.rottentomatoes.com';
-  const certifiedFresh = '/assets/pizza-pie/images/icons/tomatometer/certified_fresh-notext.56a89734a59.svg';
-  const fresh = '/assets/pizza-pie/images/icons/tomatometer/tomatometer-fresh.149b5e8adc3.svg';
-  const rotten = '/assets/pizza-pie/images/icons/tomatometer/tomatometer-rotten.f1ef4f02ce3.svg';
+  // const certifiedFresh =
+  //   '/assets/pizza-pie/images/icons/tomatometer/certified_fresh-notext.56a89734a59.svg';
+  // const fresh = '/assets/pizza-pie/images/icons/tomatometer/tomatometer-fresh.149b5e8adc3.svg';
+  // const rotten = '/assets/pizza-pie/images/icons/tomatometer/tomatometer-rotten.f1ef4f02ce3.svg';
   const rtSearch = '/search?search=';
-  const searchTerm = document.querySelector('h3.lister-item-header').innerText.replace(/^\S+/, '').trim();
+  const works = [...document.querySelectorAll('.lister-item')];
   const parser = new DOMParser();
-  GM.xmlHttpRequest({
-    method: 'GET',
-    url: rtRoot + rtSearch + searchTerm,
-    onload({ responseText }) {
-      const dom = parser.parseFromString(responseText, 'text/html');
-      const spmr = dom.querySelector('search-page-media-row');
-      console.log(spmr);
-      const state = spmr.getAttribute('tomatometerstate');
-      const score = spmr.getAttribute('tomatometerscore');
-      document.querySelector('h3.lister-item-header').innerText += `Rotten tomatoes score: ${score} (${state})`;
-    },
+  works.forEach((work) => {
+    const { title } = work.querySelector('h3.lister-item-header').innerText.match(/^(?<number>\S+) (?<title>.*) (?<year>\([^)]+\))$/).groups;
+    const topBilled = work.querySelector('.lister-item-content > p > a[href^="/name"]');
+
+    GM.xmlHttpRequest({
+      method: 'GET',
+      url: `${rtRoot}${rtSearch}${title} ${topBilled}`,
+      onload({ responseText }) {
+        const dom = parser.parseFromString(responseText, 'text/html');
+        const spmr = dom.querySelector('search-page-media-row');
+        const state = spmr.getAttribute('tomatometerstate');
+        const score = spmr.getAttribute('tomatometerscore');
+        const rtRating = document.createElement('div');
+        const rtRatingContent = document.createElement('span');
+        const rtTitle = dom.querySelector('search-page-media-row > a[slot=title]').textContent.trim();
+        rtRatingContent.innerText = `Rotten tomatoes score for ${rtTitle}: ${score} (${state})`;
+        rtRating.classList.add('inline-block');
+        rtRating.classList.add('ratings-tomatometer');
+        rtRating.appendChild(rtRatingContent);
+        work.querySelector('div.ratings-bar').appendChild(rtRating);
+      },
+    });
   });
 }());
