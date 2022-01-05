7bfae8e608ed3ed879c60cabf31e32591a4b52b7
Josh Parker <joshBBparker@gmail.com>
2022-01-04 19:06:36 -0700

    clean up pubmed dark mode (mostly ready now)

--- pubmed-central/dark-mode.user.js
+++ pubmed-central/dark-mode.user.js
@@ -11,96 +11,47 @@
 // ==/UserScript==
 
 (function darkMode() {
-  const editRules = function editRules(searchTerm, replaceTerm) {
-    if (searchTerm.constructor.name !== 'String' || replaceTerm.constructor.name !== 'String') {
-      throw new Error('string terms only');
-    }
+  const addRule = (rule) => document.styleSheets[0].insertRule(rule);
 
-    const styleSheets = [...document.styleSheets];
-    styleSheets.forEach((styleSheet, ssi) => {
-      try {
-        const cssRules = [...styleSheet.cssRules];
-        cssRules.forEach((cssRule, cri) => {
-          const { cssText } = cssRule;
-          if (cssText.includes(searchTerm)) {
-            const editedText = cssText.replace(searchTerm, replaceTerm);
-            document.styleSheets[ssi].deleteRule(cri);
-            document.styleSheets[ssi].insertRule(editedText);
-          }
-        });
-      } catch (e) {
-        console.error(e);
-        console.log('could not access css rules on styleSheet, ', styleSheet);
-      }
-    });
-  };
-
-  const searchAllStylesheets = function searchAllStylesheets(searchTerm) {
-    const constructorName = searchTerm.constructor.name;
-    const rules = [];
-    const styleSheets = [...document.styleSheets];
-    styleSheets.forEach((styleSheet) => {
-      try {
-        const { cssRules } = styleSheet;
-        [...cssRules].forEach(({ cssText }) => {
-          if ((constructorName === 'String' && cssText.includes(searchTerm))
-            || (constructorName === 'RegExp' && cssText.match(searchTerm))) {
-            rules.push(cssText);
-          }
-        });
-      } catch (e) {
-        console.error(e);
-        console.log('no css rules on ', styleSheet);
-      }
-    });
-
-    return rules;
-  };
-
-  const deleteFromMatchingRules = (termToDelete) => {
-    let re;
-    if (termToDelete instanceof String) {
-      re = new RegExp(termToDelete);
-    } else {
-      re = termToDelete;
-    }
-
-    const styleSheets = [...document.styleSheets];
-    for (let ssi = styleSheets.length - 1; ssi >= 0; ssi--) {
-      const styleSheet = styleSheets[ssi];
-      try {
-        const cssRules = [...styleSheet.cssRules];
-        for (let cri = cssRules.length - 1; cri >= 0; cri--) {
-          const cssRule = cssRules[cri];
-          const { cssText } = cssRule;
-          if (cssText.match(re)) {
-            document.styleSheets[ssi].deleteRule(cri);
-          }
-        }
-      } catch (e) {
-        console.error(e);
-        console.log('could not access css rules on styleSheet, ', styleSheet);
-      }
-    }
-  };
-
-  editRules('#ccc', '#000');
-  console.log(searchAllStylesheets('background-color'));
-  deleteFromMatchingRules('background-color');
-  const selectors = [
+  const maincontentSelectors = [
+    '#maincontent #mc',
+    '#maincontent #mc div.table-wrap',
     'body#ui-ncbiexternallink-3',
     '#ui-ncbiexternallink-3 > .grid > .col',
-    '#maincontent #mc',
+    '.bottom #footer',
+    '.bottom .contact_info',
     '#ajax-portlets div',
     '#ajax-portlets div .one_line_source',
-    '#ajax-portlets div.portlet ul > li.two_line .one_line_source, #ajax-portlets div.portlet ul > li.two_line .source',
-    '.bottom #footer, .bottom .contact_info',
+    '#ajax-portlets div.portlet ul > li.two_line .one_line_source',
+    '#ajax-portlets div.portlet ul > li.two_line .source',
+    '#ajax-portlets div.portlet ul li.expanded.highlight',
+    '.page .content #rightcolumn',
+    '.page > .content > .container > .col',
   ];
 
-  document.styleSheets[0].insertRule(`${selectors.join(', ')} { color: white; background-color: black; }`);
-  document.styleSheets[0].insertRule('.page .content a[href] { color: #BBF;}');
-  document.styleSheets[0].insertRule('.bottom #footer a[href] { color: #BBF;}');
-  document.styleSheets[0].insertRule('div.footer#footer { background: black; }');
-  document.styleSheets[0].insertRule('#footer ul > li > h3, #maincontent #mc h3, #maincontent #mc h2, #ajax-portlets .portlet h3 { color: #FDB; }');
-  document.styleSheets[0].insertRule('#maincontent #mc #pmclinksbox, .page .top .ncbi-alerts .ncbi-alert__shutdown-outer { background-color: #990; }');
+  addRule(`${maincontentSelectors.join(', ')} { color: white; background-color: black; }`);
+
+  const boxSelectors = [
+    '#maincontent #mc #pmclinksbox',
+    '.page .top .ncbi-alerts .ncbi-alert__shutdown-outer',
+    '#maincontent #mc div.iconblock',
+    '#maincontent #mc div.iconblock .icnblk_cntnt',
+  ];
+
+  addRule(`${boxSelectors.join(', ')} { color: white; background-color: #990; }`);
+  addRule(`${boxSelectors.map((bs) => `${bs} a`).join(', ')} { color: #CFF; }`);
+
+  const miscRules = [
+    '.bottom #footer a[href], .page .content a[href] { color: #BBF; }',
+    'div.footer#footer { background: black; }',
+    '#footer ul > li > h3, #maincontent #mc h3, #maincontent #mc h4, #maincontent #mc h2, #ajax-portlets .portlet h3 { color: #FDB; }',
+    '#maincontent #mc h1 { color: white; }',
+    '#maincontent #mc div.table-wrap { border: 2px solid white; }',
+    '#ajax-portlets div.pmc_para_cit { border: none; border-top: 1px solid white; }',
+    '#ajax-portlets .portlet { border-top: .4em solid #97b0c8; }',
+    '.page .content #rightcolumn .share-buttons a, .page .content #rightcolumn .format-menu ul li.selected { color: #bbb; }',
+    '.page .content .ncbi-alerts .pmc-labs-ad-outer { background-color: #055; }',
+  ];
+
+  miscRules.forEach((rule) => addRule(rule));
 }());
