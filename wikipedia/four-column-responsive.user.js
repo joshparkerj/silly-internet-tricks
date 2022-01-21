e5be2bde8e8a488a1825b4ca674ff483dd092515
Josh Parker <joshBBparker@gmail.com>
2022-01-21 13:05:33 -0700

    update four column responsive style

--- wikipedia/four-column-responsive.user.js
+++ wikipedia/four-column-responsive.user.js
@@ -11,92 +11,124 @@
 
 (function fourColumnResponsive() {
   const css = `
-div#bodyContent {
+#mw-content-text > #grid-container {
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
+  grid-template-columns: repeat(4, 1fr);
 }
 
-div#mw-content-text {
-  grid-column: span 3;
+#mw-content-text > #grid-container > .mw-parser-output {
+  min-width: 0;
+  grid-column: span 1;
   padding: 1em;
   position: relative;
   border-right: 1px solid #bbc;
 }
 
-div#mw-content-text:nth-child(7) {
-  top: 0;
+#mw-content-text > #grid-container > .mw-parser-output:nth-child(1) {
+  top: -37px;
 }
 
-div#mw-content-text:nth-child(8) {
-  top: -93vh;
+/* TODO: find a more dynamic way to calculate the top values so there is no overlap between columns */
+#mw-content-text > #grid-container > .mw-parser-output:nth-child(2) {
+  top: calc(-82vh - 37px);
 }
 
-div#mw-content-text:nth-child(9) {
-  top: -187vh;
+#mw-content-text > #grid-container > .mw-parser-output:nth-child(3) {
+  top: calc(-164vh - 37px);
 }
 
-div#mw-content-text:nth-child(10) {
-  top: -281vh;
-}
-
-div#mw-page-base {
-  position: relative;
-  z-index: 1;
-}
-
-div#mw-panel {
-  z-index: 2;
-}
-
-h1#firstHeading {
-  width: fit-content;
-}
-
-#siteSub, #contentSub, #contentSub2, #jump-to-nav {
-  display: none;
-}
-
-div#catlinks {
-  grid-column: span 12;
+#mw-content-text > #grid-container > .mw-parser-output:nth-child(4) {
+  top: calc(-246vh - 37px);
 }
 
 @media (max-width: 2000px) {
-  div#mw-content-text:nth-child(10) {
+  #mw-content-text > #grid-container > .mw-parser-output:nth-child(4) {
     display: none;
   }
 
-  div#mw-content-text {
-    grid-column: span 4;
+  #mw-content-text > #grid-container {
+    grid-template-columns: repeat(3, 1fr);
   }
 }
 
 @media (max-width: 1500px) {
-  div#mw-content-text:nth-child(9) {
+  #mw-content-text > #grid-container > .mw-parser-output:nth-child(3) {
     display: none;
   }
 
-  div#mw-content-text {
-    grid-column: span 6;
+  #mw-content-text > #grid-container {
+    grid-template-columns: repeat(2, 1fr);
   }
 }
 
 @media (max-width: 1000px) {
-  div#mw-content-text:nth-child(8) {
+  #mw-content-text > #grid-container > .mw-parser-output:nth-child(2) {
     display: none;
   }
 
-  div#mw-content-text {
-    grid-column: span 12;
+  #mw-content-text > #grid-container {
+    grid-template-columns: repeat(1, 1fr);
   }
+}
+
+div#mw-panel,
+div#mw-head,
+div#mw-page-base,
+div#siteSub,
+h1#firstHeading {
+    z-index: 1;
+}
+
+div#mw-page-base,
+div#siteSub,
+h1#firstHeading {
+    position: relative;
+}
+
+div#siteSub,
+h1#firstHeading {
+    background-color: white;
+}
+
+div#siteSub,
+h1#firstHeading {
+  padding-top: 21px;
+}
+
+div#siteSub {
+  top: -29px;
+}
+
+h1#firstHeading {
+    top: -21px;
+    border-top: 1px solid #a7d7f9
 }
 `;
 
-  const style = document.createElement('style');
-  style.appendChild(new Text(css));
+  const gridContainer = document.createElement('div');
+  gridContainer.id = 'grid-container';
 
   const mct = document.querySelector('div#mw-content-text');
-  mct.insertAdjacentElement('afterend', style);
-  mct.insertAdjacentElement('afterend', mct.cloneNode(true));
-  mct.insertAdjacentElement('afterend', mct.cloneNode(true));
-  mct.insertAdjacentElement('afterend', mct.cloneNode(true));
+  mct.appendChild(gridContainer);
+
+  const mpo = mct.querySelector('.mw-parser-output');
+  mpo.querySelectorAll('.wikitable').forEach((wikitable) => {
+    const tableContainer = document.createElement('div');
+    tableContainer.classList.add('table-container');
+    tableContainer.appendChild(wikitable.cloneNode(true));
+    tableContainer.style.setProperty('max-width', '100%');
+    tableContainer.style.setProperty('overflow', 'auto');
+    wikitable.insertAdjacentElement('afterend', tableContainer);
+    wikitable.parentNode.removeChild(wikitable);
+  });
+
+  for (let i = 0; i < 4; i++) {
+    gridContainer.appendChild(mpo.cloneNode(true));
+  }
+
+  mpo.parentNode.removeChild(mpo);
+
+  const style = document.createElement('style');
+  style.appendChild(new Text(css));
+  gridContainer.appendChild(style);
 }());
