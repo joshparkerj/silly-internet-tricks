5abbf8ee8521c58cad8f162df4d2c5a040ec15a0
Josh Parker <joshBBparker@gmail.com>
2022-01-25 21:04:22 -0700

    restyled reviews init commit (todo: make responsive)

--- README.md
+++ README.md
@@ -42,6 +42,7 @@
 38. [Three Column Reading](britannica/three-column.user.js)
 39. [Four Column Responsive](wikipedia/four-column-responsive.user.js)
 40. [Google More Results](google/more-results.user.js)
+41. [Restyled goodreads reviews](goodreads/restyled-reviews.user.js)
 
 ## All of the above are userscripts
 Run them with tampermonkey on chrome. Installation instructions are at https://www.tampermonkey.net/faq.php#Q102
---
+++ goodreads/restyled-reviews.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name         Restyled goodreads reviews
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Try splashing reviews all across four columns
+// @author       Josh Parker
+// @match        https://www.goodreads.com/book/show/*
+// @icon         https://www.google.com/s2/favicons?domain=goodreads.com
+// @grant        none
+// ==/UserScript==
+
+(function restyledReviews() {
+  const css = `
+div#lazy_loadable_view {
+  position: absolute;
+  top: 80rem;
+  left: 0rem;
+  right: 0rem;
+  padding: 1rem;
+}
+
+.rightContainer {
+  position: absolute;
+  float: none;
+  display: flex;
+  top: 44rem;
+  width: 82%;
+  left: 21rem;
+  justify-content: space-around;
+}
+
+.bookCarousel ul {
+  display: grid;
+  width: fit-content;
+  grid-template-columns: repeat(6, 1fr);
+  justify-items: center;
+}
+
+.bookCarousel {
+  height: 26rem;
+  width: 52rem;
+}
+
+.rightContainer > div {
+  margin: 1rem;
+}
+
+div[data-react-class$=GoogleBannerAd], div[data-react-class$=GoogleFeaturedContentModule] {
+  display: none;
+}
+
+div#bookReviews {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+}
+
+div#bookReviews .elementListBrownSeparator {
+  display: none;
+}
+
+footer.responsiveSiteFooter {
+  position: absolute;
+  left: 0;
+  right: 0;
+}
+
+div[id^=relatedWorks] ~ .stacked .bigBoxContent.containerWithHeaderContent {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+  `;
+
+  const style = document.createElement('style');
+  style.appendChild(new Text(css));
+  document.querySelector('body').appendChild(style);
+
+  const lazyLoadableView = document.querySelector('div#lazy_loadable_view');
+
+  const llvHeight = lazyLoadableView.clientHeight;
+
+  document.querySelector('footer.responsiveSiteFooter').style.setProperty('top', `${1260 + llvHeight}px`);
+}());
