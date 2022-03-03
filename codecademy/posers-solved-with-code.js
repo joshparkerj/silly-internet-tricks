5b90c67a71eacf120c1bc82682ac91109692d75b
Josh Parker <joshBBparker@gmail.com>
2022-03-02 20:33:02 -0700

    add scenario description

--- codecademy/posers-solved-with-code.js
+++ codecademy/posers-solved-with-code.js
@@ -1,4 +1,10 @@
 // link to poser: https://discuss.codecademy.com/t/can-this-problem-be-solved-with-code/651491/4
+
+// A game is played by 3 players in which the one who loses must double the amount of money that
+// each of the other 2 players has at that time.
+// Each of the 3 players loses 1 game and at the conclusion of the 3 games each man has $16.
+// How much money did each man start with?
+
 const reverseSimGame = (loserOfEachRound, finalDistributionOfCash) => (
   loserOfEachRound
 
@@ -17,7 +23,7 @@ const reverseSimGame = (loserOfEachRound, finalDistributionOfCash) => (
 
         const amountPaidByLoser = roundResult.reduce(
           (runningTotal, playerCash, playerNumber) => (
-            playerNumber === roundLoser ? runningTotal : runningTotal + playerCash
+            playerNumber === roundLoser ? runningTotal : runningTotal + playerCash / 2
           ),
           0,
         );
@@ -25,7 +31,7 @@ const reverseSimGame = (loserOfEachRound, finalDistributionOfCash) => (
         // return the calculated result of the previous round
         return roundResult.map(
           (playerCash, playerNumber) => (
-            playerNumber === roundLoser ? playerCash + amountPaidByLoser / 2 : playerCash / 2
+            playerNumber === roundLoser ? playerCash + amountPaidByLoser : playerCash / 2
           ),
         );
       },
