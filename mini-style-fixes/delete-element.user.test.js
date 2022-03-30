e23f4d38cc017deb85dbf46fda414f0ea515ae22
Josh Parker <superfakek@gmail.com>
2022-03-29 19:57:08 -0600

    Update delete-element.user.test.js

--- mini-style-fixes/delete-element.user.test.js
+++ mini-style-fixes/delete-element.user.test.js
@@ -9,3 +9,13 @@ describe('Google', () => {
     await expect(page.title()).resolves.toMatch('Google');
   });
 });
+
+describe('Wikipedia', () => {
+  beforeAll(async () => {
+    await page.goto('https://en.wikipedia.org');
+  });
+  
+  it('should load page' , async () => {
+    await expect(page.title()).resolves.toMatch('Wikipedia');
+  });
+});
