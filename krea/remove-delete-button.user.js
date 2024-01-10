3bd5d4b9e2fc3fc6fdb2d1f33dd1d1ffbebfe88a
Josh Parker <joshBBparker@gmail.com>
2024-01-10 08:11:22 -0700

    remove delete button from krea image enhance page

---
+++ krea/remove-delete-button.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Remove Delete Button
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-10
+// @description  Avoid accidentally deleting enhanced images by removing the delete buttons entirely
+// @author       Josh Parker
+// @match        https://www.krea.ai/apps/image/enhancer
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=krea.ai
+// @grant        none
+// ==/UserScript==
+
+(function removeDeleteButton() {
+ const style = document.createElement('style');
+ style.appendChild(new Text('button.absolute[type=submit] { display: none }'));
+ document.body.appendChild(style);
+}());
