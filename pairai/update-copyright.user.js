bfdfa5d767038584f6dcfa6f0ed33d3f6e9ddc35
Josh Parker <joshBBparker@gmail.com>
2024-01-22 09:42:37 -0700

    add copyright updater

---
+++ pairai/update-copyright.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         update copyright for pairai
+// @description  update copyright for pairai
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-22
+// @match        https://pairai.com/
+// ==/UserScript==
+(function updateCopyright() {
+ const copyrightElement = document.querySelector('[data-framer-name=links]').parentElement.children[0].querySelector('p');
+ const updatedText = copyrightElement.textContent.replace('2023', '2024');
+ copyrightElement.textContent = updatedText;
+}());
